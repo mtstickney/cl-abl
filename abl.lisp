0efa8d63c6ad1f2ba679498600b42a845a4711c1
Matthew Stickney <fiendishlinux@gmail.com>
2012-06-19 17:44:36 -0400

    Add AST functions

--- abl.lisp
+++ abl.lisp
@@ -13,14 +13,25 @@
 (defmacro delay (expr) `(lambda () ,expr))
 (defun force (thunk) (funcall thunk))
 
+(defun ast-element (node)
+  (car node))
 (defun indentation-string ()
   (make-sequence 'string (* *INDENT-LEVEL* *INDENT-SIZE*) :initial-element #\Space))
+(defun seq (&rest r)
+  "Join a sequence of statement results together."
+  (cons 'seq r))
+
+(defun abl-var (type name &key (undo nil))
+  (list 'var type name undo))
 
 (defun print-with-indent (destination format-string &rest r)
   (format destination "~a~a"
           (indentation-string)
           (apply #'format `(nil ,format-string ,@r))))
 
+(defun vars (&rest r)
+  (seq (map 'list (lambda (vdecl)
+                    (funcall #'abl-var vdecl)))))
 (defmacro with-indent (n &body body)
   `(progn
      (incf *INDENT-LEVEL* ,n)
@@ -41,83 +52,22 @@
             body)))
 
 (defun abl-parm (direction type name &key (undo nil))
-  (print-with-indent t "DEFINE ~a PARAMETER ~a AS ~a~a.~%"
-                     (force direction)
-                     (force name)
-                     (force type)
-                     (if undo ""
-                       " NO-UNDO")))
+  (list 'parm direction type name undo))
+(defun parms (&rest r)
+  (seq
+   (map 'list (lambda (pdecl)
+                (funcall #'abl-parm pdecl)))))
+(defun abl-procedure (name parm-list var-list &rest r)
+  (list 'proc name (funcall #'parms parm-list)
+         (funcall #'vars var-list)
+         r))
 
-(defun abl-parm-list (parm-list)
-  (mapc (lambda (p)
-          (abl-parm (delay (first p)) (delay (second p)) (delay (third p))))
-        parm-list))
+(defun abl-assign (&rest r)
+  (cons 'assign r))
 
-;; syntax adjustment
-(defmacro parms (&body body)
-  `(abl-parm-list (list ,@(map 'list (lambda (l) (cons 'list l))) body)))
+(defun abl-call (name parm-list)
+  (list 'call name (funcall #'parms parm-list)))
 
-(defun abl-procedure (name param-list &rest r)
-  (print-with-indent "PROCEDURE ~a:~%" (force name))
-  (with-indent 1
-               (abl-parm-list param-list)
-               (mapc #'force r))
-  (print-with-indent "END.~%"))
-
-;; (defmacro procedure (name param-list &body body)
-;;   `(abl-procedure (delay ,name) (delay ,param-list))
-;;   )
-
-;; (defun statement (s)
-;;   (lambda ()
-;;     (print-with-indent nil "~a.~%" (funcall s))))
-
-;; (defun define-var (name type &key (undo nil))
-;;   (statement
-;;    (lambda ()
-;;      (print-with-indent nil "~{~a~^ ~}~a"
-;;                         `(define variable ,(funcall name) as ,(funcall type))
-;;                         (if (not undo)
-;;                             " NO-UNDO"
-;;                           "")))))
-
-;; (defun define-parm (name type parm-type&key (undo nil))
-;;   (unless (eql parm-type :output :input :input-output :return))
-;;   (let ((parm-type (cond
-;;                ((not (and input-p input))
-;;                 :output)
-;;                ((not (and output-p output))
-;;                 :input)
-;;                (t
-;;                 :input-output))))
-;;     (statement
-;;      (lambda ()
-;;        (print-with-indent nil "~{~a~^ ~}~a"
-;;                           `(define ,parm-type parameter ,(funcall name) as ,(funcall type))
-;;                           (if (not undo)
-;;                               " NO-UNDO"
-;;                             ""))))))
-
-;; (defun seq (&rest r)
-;;   (lambda ()
-;;     (apply #'concatenate 'string  (map 'list #'funcall r))))
-
-;; (defun dvar (name type &key (undo nil))
-;;   (define-var (lambda () name) (lambda () type) :undo undo))
-
-;; (defun dparm (name type &rest r)
-;;   (apply #'define-parm `(,(lambda () name) ,(lambda () type) ,@r)))
-
-;; (defun with-block (func var-list &rest body)
-;;   "Takes a list of lists, each of which are the arguments to func, and a body of code to follow."
-;;   (apply #'seq `(,@(map 'list (lambda (args) (apply func args)) var-list)
-;;                  ,(lambda () (make-sequence 'string 1 :initial-element #\Newline))
-;;                  ,@body)))
-
-;; (defun with-vars (var-list &rest body)
-;;   (with-block #'dvar var-list body))
-
-;; (defun with-parms (parm-list &rest body)
-;;   (with-block #'dparm parm-list body))
-
-;; (defun assign ())
+(defun abl-handler-block (form &rest handler-list)
+  (list 'with-handlers form
+        (map 'list #'abl-handler handler-list)))
